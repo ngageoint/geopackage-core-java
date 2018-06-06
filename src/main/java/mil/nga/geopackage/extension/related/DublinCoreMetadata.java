83dea102a136e785d2b6b32aa50f2b29d8b10723
Brian Osborn <bosborn@users.noreply.github.com>
2018-06-06 10:55:18 -0600

    Dublin Core Metadata support for rows and tables

---
+++ src/main/java/mil/nga/geopackage/extension/related/DublinCoreMetadata.java
@@ -0,0 +1,113 @@
+package mil.nga.geopackage.extension.related;
+
+import mil.nga.geopackage.user.UserColumn;
+import mil.nga.geopackage.user.UserCoreRow;
+import mil.nga.geopackage.user.UserTable;
+
+/**
+ * Dublin Core Metadata Initiative
+ * 
+ * @author osbornb
+ * @since 3.0.1
+ */
+public class DublinCoreMetadata {
+
+	/**
+	 * Check if the table has a column for the Dublin Core Type term
+	 * 
+	 * @param table
+	 *            user table
+	 * @param type
+	 *            Dublin Core Type
+	 * @return true if has column
+	 */
+	public static boolean hasColumn(UserTable<?> table, DublinCoreType type) {
+
+		boolean hasColumn = table.hasColumn(type.getName());
+
+		if (!hasColumn) {
+			for (String synonym : type.getSynonyms()) {
+				hasColumn = table.hasColumn(synonym);
+				if (hasColumn) {
+					break;
+				}
+			}
+		}
+
+		return hasColumn;
+	}
+
+	/**
+	 * Check if the row has a column for the Dublin Core Type term
+	 * 
+	 * @param row
+	 *            user row
+	 * @param type
+	 *            Dublin Core Type
+	 * @return true if has column
+	 */
+	public static boolean hasColumn(UserCoreRow<?, ?> row, DublinCoreType type) {
+		return hasColumn(row.getTable(), type);
+	}
+
+	/**
+	 * Get the column from the table for the Dublin Core Type term
+	 * 
+	 * @param table
+	 *            user table
+	 * @param type
+	 *            Dublin Core Type
+	 * @return column
+	 */
+	public static <T extends UserColumn> T getColumn(UserTable<T> table,
+			DublinCoreType type) {
+
+		T column = null;
+
+		if (table.hasColumn(type.getName())) {
+			column = table.getColumn(type.getName());
+		} else {
+			for (String synonym : type.getSynonyms()) {
+				if (table.hasColumn(synonym)) {
+					column = table.getColumn(synonym);
+					break;
+				}
+			}
+		}
+
+		return column;
+	}
+
+	/**
+	 * Get the column from the row for the Dublin Core Type term
+	 * 
+	 * @param row
+	 *            user row
+	 * @param type
+	 *            Dublin Core Type
+	 * @return column
+	 */
+	public static <T extends UserColumn> T getColumn(UserCoreRow<T, ?> row,
+			DublinCoreType type) {
+		return getColumn(row.getTable(), type);
+	}
+
+	/**
+	 * Get the value from the row for the Dublin Core Type term
+	 * 
+	 * @param row
+	 *            user row
+	 * @param type
+	 *            Dublin Core Type
+	 * @return value
+	 */
+	public static Object getValue(UserCoreRow<?, ?> row, DublinCoreType type) {
+
+		UserColumn column = getColumn(row, type);
+
+		Object value = row.getValue(column.getIndex());
+
+		return value;
+	}
+
+}
---
+++ src/main/java/mil/nga/geopackage/extension/related/DublinCoreType.java
@@ -0,0 +1,103 @@
+package mil.nga.geopackage.extension.related;
+
+/**
+ * Dublin Core Metadata Initiative term types
+ * 
+ * @author osbornb
+ * @since 3.0.1
+ */
+public enum DublinCoreType {
+
+	/**
+	 * A point or period of time associated with an event in the lifecycle of
+	 * the resource.
+	 */
+	DATE("date"),
+
+	/**
+	 * An account of the resource.
+	 */
+	DESCRIPTION("description"),
+
+	/**
+	 * The file format, physical medium, or dimensions of the resource.
+	 */
+	FORMAT("format", "content_type"),
+
+	/**
+	 * An unambiguous reference to the resource within a given context.
+	 */
+	IDENTIFIER("identifier", "id"),
+
+	/**
+	 * A related resource from which the described resource is derived.
+	 */
+	SOURCE("source"),
+
+	/**
+	 * A name given to the resource.
+	 */
+	TITLE("title");
+
+	/**
+	 * Term type name
+	 */
+	private final String name;
+
+	/**
+	 * Term type synonyms
+	 */
+	private final String[] synonyms;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param name
+	 *            term name
+	 * @param synonyms
+	 *            synonymous column names
+	 */
+	private DublinCoreType(String name, String... synonyms) {
+		this.name = name;
+		this.synonyms = synonyms;
+	}
+
+	/**
+	 * Get the term name
+	 * 
+	 * @return term name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Get the synonymous column names
+	 * 
+	 * @return synonyms
+	 */
+	public String[] getSynonyms() {
+		return synonyms;
+	}
+
+	/**
+	 * Get the Dublin Core Type from the term name
+	 * 
+	 * @param name
+	 *            term name
+	 * @return Dublin Core type
+	 */
+	public static DublinCoreType fromName(String name) {
+		DublinCoreType dublinCoreType = null;
+		if (name != null) {
+			for (DublinCoreType type : DublinCoreType.values()) {
+				if (name.equals(type.getName())) {
+					dublinCoreType = type;
+					break;
+				}
+			}
+		}
+		return dublinCoreType;
+	}
+
+}
